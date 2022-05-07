ce7dab0d8d7a3fa67140babb9dd036058fd3ee77
Chiranth A J <chiranth@m.ms.evolution-x.org>
2022-05-07 22:03:04 +0530

    Drop Prebuilt overlay apk for ThemedIcons

--- config/common.mk
+++ config/common.mk
@@ -115,10 +115,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     CustomPixelLauncherOverlay
 
-# Prebuilt
-PRODUCT_PACKAGES += \
-    ThemedIconsOverlay
-
 # One Handed mode
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.support_one_handed_mode=true \
