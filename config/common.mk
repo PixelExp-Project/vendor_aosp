e60b02d47fea9aae04f9c45dba93e8077875ec61
Chiranth A J <chiranth@m.ms.evolution-x.org>
2022-05-07 22:01:10 +0530

    ThemedIconsOverlay: Import resources from TeamFiles(https://t.me/modulesrepo)
    Signed-off-by: Chiranth A J <chiranth@m.ms.evolution-x.org>

--- config/common.mk
+++ config/common.mk
@@ -104,6 +104,10 @@ PRODUCT_PACKAGE_OVERLAYS += \
 #PRODUCT_PACKAGES += \
 #    TouchGestures
 
+# Themed Icons
+PRODUCT_PACKAGES += \
+    CustomPixelLauncherOverlay
+
 # One Handed mode
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.support_one_handed_mode=true \
